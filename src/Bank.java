50b897368bab03ab1c47086043f3ae5511409493
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-02-08 11:43:37 +0100

    added first showBankMenu method

--- src/Bank.java
+++ src/Bank.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Bank {
     // Attributs
     String name;
     int capital = 100000;
     List<String> transferHistory = new ArrayList<>();
+    Client client;
 
     // Constructeur
 
@@ -37,6 +39,24 @@ public class Bank {
         System.out.println("Voici la liste des transfers qui ont eut lieu dans " + this.name);
         for (int i = 0; i < transferHistory.size(); i++) {
             System.out.println(transferHistory.get(i));
+        }
+    }
+
+    void showBankMenu() {
+        System.out.println("Bienvenu guichetière, que voulez vous faire ?:");
+        System.out.println("1 - Voir la liste des clients");
+        System.out.println("2 - Voir la liste des comptes");
+        System.out.println("3 - Effectuer un versement");
+        System.out.println("4 - Effectuer un retrait");
+        System.out.println("5 - Effectuer un virement entre deux clients");
+        Scanner sc = new Scanner(System.in);
+        String nbTodo = sc.nextLine();
+        while (nbTodo != "1" || nbTodo != "2"|| nbTodo != "3"|| nbTodo != "4"|| nbTodo != "5") {
+            System.out.println("Erreur de saisie, entrez le numéro correspondant à une action");
+        }
+        switch (nbTodo) {
+            case "1":
+                System.out.println();
         }
     }
 }
