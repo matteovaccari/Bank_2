e353a03c6ac1c36a904b342cd67f4080a4427f62
Matteo VACCARI <matteo.vaccari@yahoo.com>
2019-02-13 16:28:18 +0100

    modified exception --> insuBalance --> negative amount typo

--- src/com/vaccari/matteo/account/exceptions/InsufisiantBalanceForWithdrawalException.java
+++ src/com/vaccari/matteo/account/exceptions/InsufisiantBalanceForWithdrawalException.java
@@ -3,6 +3,6 @@ package com.vaccari.matteo.account.exceptions;
 public class InsufisiantBalanceForWithdrawalException extends Exception {
 
     public InsufisiantBalanceForWithdrawalException() {
-        System.out.println("Somme négative pour retrait, erreur.");
+        System.out.println("Solde insufisant pour retrait, erreur.");
     }
 }
